d4c95f7fcbe7204ed562da3dc68cfd1b5dd9f4ce
rasmontas <rimantas.asmontas@gmail.com>
2023-09-06 16:00:55 +0300

    Initial commit

---
+++ src/main/java/lt/rimas/Namu/Biblioteka/NamuBibliotekaApplication.java
@@ -0,0 +1,13 @@
+package lt.rimas.Namu.Biblioteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamuBibliotekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamuBibliotekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/rimas/Namu/Biblioteka/NamuBibliotekaApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.rimas.Namu.Biblioteka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamuBibliotekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
